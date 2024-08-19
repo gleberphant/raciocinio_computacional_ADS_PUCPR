5cd23f0c8be20bcd7c166e9b7dbe93ffaa4d8c72
gleberphant <cartoladosama@gmail.com>
2024-08-19 10:43:13 -0300

    ajustes gerais

--- main_s7_af.py
+++ main_s7_af.py
@@ -1,110 +1,97 @@
-"""SITEMA DE GESTÃO ACADÊMICA"""
+""" sitema de gestão academica - pucpr VS6"""
+# ┌─────────────────────────────────────────────────────────────┐
+# │         ╔════╗           ╔═════     ╔════╗  ╔═══╗           │
+# │         ║    ║  ║    ║   ║          ║    ║  ║   ║           │
+# │         ╠════╝  ║    ║   ║          ╠════╝  ╠═══╩╗          │
+# │         ║       ╚════╝   ╚═════     ║       ║    ║          │
+# └─────────────────────────────────────────────────────────────┘
+# ┌─────────────────────────────────────────────────────────────┐
+# │  CURSO: Análise e Desenvolvimento de Sistemas               │
+# │  DISCIPLINA: Raciocínio Computacional (11100010563_20242_02)│
+# │  ALUNO: HANDERSON GLEBER DE LIMA CAVALCANTI (Gr4v4t1nh4)    │
+# │  MATRÍCULA: 1112024201103                                   │
+# └─────────────────────────────────────────────────────────────┘
+# ┌─────────────────────────────────────────────────────────────┐
+# │   Atividade FORMATIVA 4 - semana 6                          │
+# └─────────────────────────────────────────────────────────────┘
+# CRITÉRIOS
+# -	Você deve obrigatoriamente modularizar (colocar dentro de funções) as seguintes funcionalidades:
+# -	Apresentação do Menu Principal
+# -	Apresentação do Menu de Operações
+# -	Inclusão de estudante
+# -	Listagem de estudantes
+# - Edição de estudante
+# -	Exclusão de estudante
 
-# ██████  ██    ██  ██████     ██████  ██████
-# ██   ██ ██    ██ ██          ██   ██ ██   ██
-# ██████  ██    ██ ██          ██████  ██████
-# ██       ██████   ██████     ██      ██   ██
 
-# Curso: Análise e Desenvolvimento de Sistemas
-# Disciplina: Raciocínio Computacional (11100010563_20242_02)
-# Aluno: HANDERSON GLEBER DE LIMA CAVALCANTI (Gr4v4t1nh4)
-# MATRÍCULA 1112024201103
-
-# Atividade Formativa num 7
-# Função para salvar lista de estudantes em um arquivo JSON.
-# Função para recuperar lista de estudantes de um arquivo JSON e armazenar em uma variável em memória.
-# Adaptação das funções de incluir, listar, excluir e editar estudantes para acessarem as duas funções acima
-# sempre que necessário.
-# Esta funcao desenha o menu principal da aplicação. Coloquei em uma função para não poluir o código principal
-
-
+#  Esta funcao desenha o menu principal da aplicação. Coloquei em uma função para não poluir o código principal
 def mostra_menu_principal():
-    """função para mostrar o menu principal. return none"""
-
-    print("\n")
-    print("\t -------------- MENU PRINCIPAL --------------- ")
-    print("\t|                                             |")
-    print("\t| (1) Gerenciar Estudantes.                   |")
-    print("\t| (2) Gerenciar Professores.                  |")
-    print("\t| (3) Gerenciar Disciplinas.                  |")
-    print("\t| (4) Gerenciar Turmas.                       |")
-    print("\t| (5) Gerenciar Matrícula.                    |")
-    print("\t|                                             |")
-    print("\t| (9) Sair.                                   |")
-    print("\t|                                             |")
-    print("\t --------------------------------------------- ")
-    print("\n")
+    print("┌───────────────[ MENU PRINCIPAL ]──────────────┐")
+    print("│                                               │")
+    print("│ (1) Gerenciar Estudantes.                     │")
+    print("│ (2) Gerenciar Professores.                    │")
+    print("│ (3) Gerenciar Disciplinas.                    │")
+    print("│ (4) Gerenciar Turmas.                         │")
+    print("│ (5) Gerenciar Matrícula.                      │")
+    print("│                                               │")
+    print("│ (9) Sair.                                     │")
+    print("│                                               │")
+    print("└───────────────────────────────────────────────┘")
     return None
+
 
 # Esta funcao desenha o menu secundário da aplicação. Coloquei em uma função para não poluir o código principal
 # ela recebe como parametro o menu secundário que é para ser desenhado. Tendo como valor padrão o menu ESTUDANTE
-
-
 def mostra_submenu():
-    """função para mostrar o menu secundario. return none"""
-
-    print("\n")
-    print("\t **********************************************")
-    print("\t *                 MENU OPERAÇÕES             *")
-    print("\t *                                            *")
-    print("\t * [(1) Incluir ]                             *")
-    print("\t * [(2) Listar  ]                             *")
-    print("\t * [(3) Editar  ]                             *")
-    print("\t * [(4) Excluir ]                             *")
-    print("\t *                                            *")
-    print("\t * [(9) Voltar  ]                             *")
-    print("\t *                                            *")
-    print("\t **********************************************")
-    print("\n")
+    print("┌──────────────[ MENU OPERAÇÕES ]───────────────┐")
+    print("│                                               │")
+    print("│ (1) Incluir.                                  │")
+    print("│ (2) Listar.                                   │")
+    print("│ (3) Atualizar.                                │")
+    print("│ (4) Excluir.                                  │")
+    print("│                                               │")
+    print("│ (9) Voltar                                    │")
+    print("│                                               │")
+    print("└───────────────────────────────────────────────┘")
     return None
+
+
 # Esta funcao desenha a mensagem de opção inválida. Coloquei em uma função para não poluir o código principal
-
-
 def msg_opcao_invalida():
-    """Function to show msg"""
-    print("\t -----------------------------------------------")
-    print("\t !  OPÇÃO INVÁLIDA                             !")
-    print("\t -----------------------------------------------")
+    print("\t╔════════════════════╗")
+    print("\t║   OPÇÃO INVÁLIDA   ║")
+    print("\t╚════════════════════╝")
     return None
 
 
 # Esta funcao desenha a mensagem de saida da aplicação. Coloquei em uma função para não poluir o código principal
 def msg_saida():
-    """Function to show msg"""
-    print("\n \n")
-    print("\t --------------------------------------------")
-    print("\t .       Dúvidas e sugestões?               .")
-    print("\t .       Entre em contato por               .")
-    print("\t .       handerson.gleber@gmail.com         .")
-    print("\t --------------------------------------------")
-    print("\n \n")
+    print(" ╔════════════════════════════════════════════╗")
+    print(" ║      Dúvidas e sugestões?                  ║")
+    print(" ║      Entre em contato por                  ║")
+    print(" ║      handerson.gleber@gmail.com            ║")
+    print(" ╚════════════════════════════════════════════╝")
     return None
 
 
 # Esta funcao desenha a mensagem de abertura da aplicação. Coloquei em uma função para não poluir o código principal
 def msg_abertura():
-    """Function to show msg"""
-    print("\n \n")
-    print("\t --------------------------------------------")
-    print("\t ............................................")
-    print("\t ....... SISTEMA DE GESTÃO ACADÊMICA.........")
-    print("\t ............................................")
-    print("\t .............por: Gravatinha................")
-    print("\t ............................................")
-    print("\t --------------------------------------------")
-    print("\n \n")
-
+    print("")
+    print(" ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░")
+    print(" ░░░░░░░░░░SISTEMA DE GESTÃO ACADÊMICA░░░░░░░░")
+    print(" ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░")
+    print(" ░░░░ por: HANDERSON GLEBER (gravatinha) ░░░░░")
+    print(" ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░")
+    print("")
     return None
 
 
 def msg_enter_continua():
-    """Function to show msg. return none"""
     input("\t Pressione <ENTER> para continuar ...")
     return None
 
 
 def popula_dados_teste():
-    """Function to show msg. return none"""
     lista = []
     for i in range(8):
         lista.append({"codigo": 1 * i, "nome": "aluno_" + str(i), "cpf": str(i) * 6})
@@ -112,7 +99,7 @@ def popula_dados_teste():
 
 
 def inserir_item(novo_item, lista_param):
-    """Function CRUD -> create item. return list """
+
     lista_param.append(novo_item)
     print(f"\t Item inserido com sucesso: \n \t {1} ", novo_item)
 
@@ -120,18 +107,20 @@ def inserir_item(novo_item, lista_param):
 
 
 def listar_itens(lista_param):
-    """Function CRUD -> read item. return none """
-    print("\t --------------------------------------------------")
+    print("")
+    print("┌───────────────────────────────────────────────┐")
+    print("│           LISTAR ITENS CADASTRADOS            │")
     for item in lista_param:
+        print("│-----------------------------------------------│")
         for k, v in item.items():
-            print(f"\t |{k}\t: {v}  ")
-        print("\t_______________________________")
-    print("\t --------------------------------------------------")
+            print(f"│ {k:<15} : {v:<25}   │")
+
+    print("│                                               │")
+    print("└───────────────────────────────────────────────┘")
     return None
 
 
 def buscar_item(lista_param):
-    """Function CRUD -> search item. return item """
     codigo_busca = input("Digite o Código do Item : ")
     if codigo_busca.isnumeric() is True:
         for item in lista_param:
@@ -141,14 +130,15 @@ def buscar_item(lista_param):
 
 
 def remover_item(lista_param):
-    """Function CRUD -> remove item. return list """
-
     item_encontrado = buscar_item(lista_param)
 
     if item_encontrado is not False:
         if input("\t Voce tem certeza que deseja excluir ? ").upper() == "S":
+
             lista_param.remove(item_encontrado)
+
             print("\t Código  excluido com sucesso")
+
     else:
         print("\t Nenhum item foi encontrado com ese codigo")
 
@@ -156,7 +146,6 @@ def remover_item(lista_param):
 
 
 def editar_item(lista_param):
-    """Function CRUD -> update item. return list """
 
     item_encontrado = buscar_item(lista_param)
 
@@ -171,7 +160,6 @@ def editar_item(lista_param):
 
 
 def main():
-    """Funçao principal"""
 
     msg_abertura()  # chama mensagem de abertura da aplicação
 
@@ -182,8 +170,7 @@ def main():
 
         mostra_menu_principal()  # chama o desenho do menu principal
 
-        opcao_menu_principal = input(
-            "\t Informe o numero da opção desejada:  ")[0]
+        opcao_menu_principal = input("\t Informe o numero da opção desejada:  ")[0]
 
         if opcao_menu_principal == "1":  # ENTRA NO MENU DE ESTUDANTE
 
@@ -191,8 +178,7 @@ def main():
 
                 mostra_submenu()
 
-                opcao_submenu1 = input(
-                    "\t Informe o número da opção desejada: ")
+                opcao_submenu1 = input("\t Informe o número da opção desejada: ")
 
                 match opcao_submenu1:
                     case "1":  # Opção de incluir
@@ -231,9 +217,9 @@ def main():
                         msg_opcao_invalida()
 
         elif (opcao_menu_principal == "2"
-                or opcao_menu_principal == "3"
-                or opcao_menu_principal == "4"
-                or opcao_menu_principal == "5"):  # OUTROS MODULOS DO MENU PRINCIPAL
+              or opcao_menu_principal == "3"
+              or opcao_menu_principal == "4"
+              or opcao_menu_principal == "5"):  # OUTROS MODULOS DO MENU PRINCIPAL
 
             print("\t *** EM DESENVOLVIMENTO **** ")
             msg_enter_continua()
